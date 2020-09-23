10656e2ae270642077758adb5419b52a127d66f6
Vladislav <Gaemvlad@gmail.com>
2020-09-24 00:54:23 +0500

    Init Repo

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Create from '../views/Create.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'Create',
+        component: Create
+    },
+    {
+        path: '/list',
+        name: 'List',
+        component: () =>
+            import ('@/views/List.vue'),
+    },
+    {
+        path: '/task/:id',
+        name: 'Task',
+        component: () =>
+            import ('@/views/Task.vue'),
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
